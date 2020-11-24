ecb300cacae7a1ac1440fd04c02315287aa44394
sabrina hamlaoui <sabrinahamlaoui@outlook.com>
2020-11-24 14:42:42 +0100

    React-check-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
+import img from "./image1.jpg"
+import img2 from "./image2.jpg"
 import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="title red">Recette de la Forêt Noire</h1>
+      <br />
+      <img src={img2} width={300} height={200}/>
+      <br />
+      <img src={img} />
     </div>
-  );
+    <video width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </div>
+);
 }
-
 export default App;
